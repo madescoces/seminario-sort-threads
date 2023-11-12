75ddcd1b70361c738d913a810aa1c4f404155c20
lopez-sol <lopez.msol.07@gmail.com>
2023-11-12 17:07:06 -0300

    Agregado de BubbleSort

--- functions.py
+++ functions.py
@@ -82,3 +82,15 @@ def mergeSort(arr):
   [threads[th].join() for th in threads]
 
   merge(arr, parts)
+
+def bubbleSort(arr):
+	n = len(arr)
+	swapped = False
+	for i in range(n-1):
+		for j in range(0, n-i-1):
+			if arr[j] > arr[j + 1]:
+				swapped = True
+				arr[j], arr[j + 1] = arr[j + 1], arr[j]
+		
+		if not swapped:
+			return
