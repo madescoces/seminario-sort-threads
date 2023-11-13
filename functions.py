1fcd552ce08949c247feb65eb47aa2d2255e3eb6
madescoces <madescoces@gmail.com>
2023-11-13 01:27:09 -0300

    Merge, bubble, quick, todos funcionales, notebook actualizada

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
+from enum import Enum
 import threading
 import numpy as np
 import random
 
 # Función para generar un array de gaussianos
+
+
 def gaussianRandom(n: int) -> np.array:
   # Genera los números base
   uniform = [random.random() for _ in range(n * 2)]
@@ -17,22 +20,22 @@ def gaussianRandom(n: int) -> np.array:
 
   return gaussian
 
-# saca el indice medio de un array
+
 def arrMiddle(arr):
   middle = len(arr) // 2
   return middle
 
-# Primer mitad del array
+
 def arrFirstHalf(arr):
   return arr[:arrMiddle(arr)]
 
-# Segunda mitad del array
+
 def arrSecondHalf(arr):
   return arr[arrMiddle(arr):]
 
-# Función que ordena
+
 def merge(left, right):
-  arr = [0]*len(left+right)
+  arr = [0] * len(left + right)
 
   # i = indice left array, j = indice right array, k = indice array a ordenar
   index = {'i': 0, 'j': 0, 'k': 0}
@@ -59,96 +62,107 @@ def merge(left, right):
   step('j', right)
   return arr
 
-# Función que dispara los threads para el sort
-def mergeSort(arr):
-  if len(arr) <= 1:
+
+def sortThreaded(arr,  _sortType=None, threads=2):
+  sortType = _sortType if _sortType else MergeSort()  
+
+  if threads <= 1:
+    return sortType.sortMe(arr)
+
+  sublists = []
+  size = len(arr) // threads
+  for i in range(0, len(arr), size):
+    sublists.append(arr[i:i + size])
+
+  sorted_sublists = []
+  threads = []
+
+  if threads != 0:    
+    for sublist in sublists:
+      thread = threading.Thread(target=lambda sublist: sorted_sublists.append(sortType.sortMe(sublist)), args=(sublist,))
+      threads.append(thread)
+    
+    for thread in threads:
+      thread.start()
+
+    for thread in threads:
+      thread.join()
+
+    merged = sorted_sublists[0]
+    for sublist in sorted_sublists[1:]:
+      merged = merge(merged, sublist)
+    return merged  
+
+  left_half = sortThreaded(arrFirstHalf(arr))
+  right_half = sortThreaded(arrSecondHalf(arr))
+
+  return merge(left_half, right_half)
+
+
+class Sort:
+  def sortMe(self, arr):
+    pass
+
+
+class MergeSort(Sort):
+  def sortMe(self, arr):
+    return self._mergeSort(arr)
+
+  def _mergeSort(self, arr):    
+    if not len(arr) > 1:
+      return arr
+    
+    parts = [arrFirstHalf(arr), arrSecondHalf(arr)]
+    parts = [self._mergeSort(part) for part in parts]
+    return merge(*parts)
+  
+class BubbleSort(Sort):
+  def sortMe(self, arr):
+    if not len(arr) > 1:
+      return arr
+
+    n = len(arr)
+    swapped = False
+    
+    for i in range(n - 1):
+      for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+          swapped = True
+          arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+      if not swapped:
+        return arr
+    
     return arr
 
-  # diccionario con los 2 threads por cada división
-  # threads = {}
+class QuickSort(Sort):
+  def sortMe(self, arr):    
+    return self._quicksort(arr)
 
-  # Array con ambas partes
-  parts = [arrFirstHalf(arr), arrSecondHalf(arr)]
-  parts = [mergeSort(part) for part in parts]
-  return merge(*parts)
-
-  # # Crea los hilos
-  # for i, part in enumerate(parts):
-  #   threads[i] = threading.Thread(target=mergeSort, args=(part,))
-
-  # # Dispara los hilos
-  # [threads[th].start() for th in threads]
-
-  # # Espera a que finalicen todos para unirlos
-  # [threads[th].join() for th in threads]
-
-def merge_sort_threaded(arr, num_th):
-  if num_th <= 1:
-    return mergeSort(arr)
-  
-  sublists = []
-  size = len(arr) // num_th
-  for i in range(0,len(arr),size):
-    sublists.append(arr[i:i+size])
-  
-  sorted_sublists = []  
-  threads = []
-  for sublist in sublists:
-    thread = threading.Thread(target=lambda sublist: sorted_sublists.append(mergeSort(sublist)), args=(sublist,)) 
-    threads.append(thread)
-  
-  for thread in threads:        
-    thread.start()
+  def _quicksort(self, arr):       
+    if not len(arr) > 1:
+      return arr
     
-  for thread in threads:        
-    thread.join()
+    pivot = arr[0]
+    lesser = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
+    return self._quicksort(lesser) + [pivot] + self._quicksort(greater)
 
-  merged = sorted_sublists[0]
-  for sublist in sorted_sublists[1:]:
-    merged = merge(merged, sublist)
-  return merged
 
-data = gaussianRandom(16)
-print(merge_sort_threaded(data, 3))
+# data = gaussianRandom(40000)
 
-def bubbleSort(arr):
-	n = len(arr)
-	swapped = False
-	for i in range(n-1):
-		for j in range(0, n-i-1):
-			if arr[j] > arr[j + 1]:
-				swapped = True
-				arr[j], arr[j + 1] = arr[j + 1], arr[j]
-		
-		if not swapped:
-			return
+# arr = sortThreaded(data, MergeSort(), 4)
 
-#Para iniciar parle el array completo, en low 0 y en high el lenght-1 del array
-def quick_sort(array, low, high):
+# print(arr[:20])
 
-  def partition(array, low, high):  
-    # Pivote el de la derecha
-    pivot = array[high]
-    
-    # Apuntador del último elemento más pequeño
-    i = low - 1
-  
-    for j in range(low, high):
-      if array[j] <= pivot:
-        # Avanzar apuntador
-        i = i + 1
-        # Intercambiar elementos
-        (array[i], array[j]) = (array[j], array[i])
-    
-    # Al final intercambiar el pivote
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-    
-    # Regresa la posición final del pivote
-    return i + 1
-  
-  if low < high:
-    # Dividir y acomodar pivote
-    pi = partition(array, low, high)
-  
-    quick_sort(array, low, pi - 1)
-    quick_sort(array, pi + 1, high)
+
+# t1 = time.perf_counter()
+
+# sorted_list = multi_threaded_merge_sort(input_list, num_threads)
+
+# t2 = time.perf_counter()
+
+# print("Sorted list:", sorted_list[:50])
+
+# print(f"Time taken: {t2 - t1:.6f} seconds")
+
